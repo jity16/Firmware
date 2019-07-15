1455a573ed25dbc4ddab55ba294391bd5b1e5f39
bresch <brescianimathieu@gmail.com>
2019-07-15 17:57:40 +0200

    FlightTask - Rename state_prev to last_setpoint

--- src/lib/FlightTasks/tasks/ManualPositionSmoothVel/FlightTaskManualPositionSmoothVel.cpp
+++ src/lib/FlightTasks/tasks/ManualPositionSmoothVel/FlightTaskManualPositionSmoothVel.cpp
@@ -38,15 +38,15 @@
 
 using namespace matrix;
 
-bool FlightTaskManualPositionSmoothVel::activate(vehicle_local_position_setpoint_s state_prev)
+bool FlightTaskManualPositionSmoothVel::activate(vehicle_local_position_setpoint_s last_setpoint)
 {
-	bool ret = FlightTaskManualPosition::activate(state_prev);
+	bool ret = FlightTaskManualPosition::activate(last_setpoint);
 
 	// Check if the previous FlightTask provided setpoints
-	checkSetpoints(state_prev);
-	const Vector3f accel_prev = Vector3f(state_prev.acc_x, state_prev.acc_y, state_prev.acc_z);
-	const Vector3f vel_prev = Vector3f(state_prev.vx, state_prev.vy, state_prev.vz);
-	const Vector3f pos_prev = Vector3f(state_prev.x, state_prev.y, state_prev.z);
+	checkSetpoints(last_setpoint);
+	const Vector3f accel_prev = Vector3f(last_setpoint.acc_x, last_setpoint.acc_y, last_setpoint.acc_z);
+	const Vector3f vel_prev = Vector3f(last_setpoint.vx, last_setpoint.vy, last_setpoint.vz);
+	const Vector3f pos_prev = Vector3f(last_setpoint.x, last_setpoint.y, last_setpoint.z);
 
 	for (int i = 0; i < 3; ++i) {
 		_smoothing[i].reset(accel_prev(i), vel_prev(i), pos_prev(i));
